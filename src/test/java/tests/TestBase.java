bb4f78ac7b05c8ec96147711ad3981e670ef4603
artl <artemlarichev.v@gmail.com>
2025-05-11 21:48:56 +0200

    changed name of method inside @BeforAll to setupConfiguration

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -9,7 +9,7 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 public class TestBase {
 
     @BeforeAll
-    static void beforeAll() {
+    static void setupConfiguration() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
